21b6b663ca05492e7d5c1e318d1ffa1906805b93
pvecino <pvecino@users.noreply.github.com>
2018-12-11 12:21:11 +0100

    Update alicia.sh

--- PH/alicia.sh
+++ PH/alicia.sh
@@ -2,17 +2,17 @@
 wget http://www.gutenberg.org/files/11/11.txt
 mkdir /tmp/Alice/
 mv $(pwd)/11.txt /tmp/Alice/alice.txt
+grep -e "Title" -e "Author" -e "Posting Date" -e "Posting Date" -e "Language" -e "Character set encoding" /tmp/Alice/alice.txt | sort
 
+#otra opcion:
 #mkdir /tmp/Alice/
 #wget -O /tmp/Alice/alice.txt http://www.gutenberg.org/files/11/11.txt
-
-Titulo=$(cat /tmp/Alice/alice.txt | grep "Title: ")
-Autor=$(cat /tmp/Alice/alice.txt | grep "Authors: ")
-Post=$(cat /tmp/Alice/alice.txt | grep "Posting Date: ")
-Release=$(cat /tmp/Alice/alice.txt | grep "Posting Date: ")
-Language=$(cat /tmp/Alice/alice.txt | grep "Language: ")
-Character=$(cat /tmp/Alice/alice.txt | grep "Character set encoding: ")
-grep -e "Title" -e "Author" -e "Posting Date" -e "Posting Date" -e "Language" -e "Character set encoding" /tmp/Alice/alice.txt | sort
+#Titulo=$(cat /tmp/Alice/alice.txt | grep "Title: ")
+#Autor=$(cat /tmp/Alice/alice.txt | grep "Authors: ")
+#Post=$(cat /tmp/Alice/alice.txt | grep "Posting Date: ")
+#Release=$(cat /tmp/Alice/alice.txt | grep "Posting Date: ")
+#Language=$(cat /tmp/Alice/alice.txt | grep "Language: ")
+#Character=$(cat /tmp/Alice/alice.txt | grep "Character set encoding: ")
 #echo $Autor
 #echo $Character
 #echo $Language
